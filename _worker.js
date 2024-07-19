b7cc42c902dff985e21e321b20ad87c95f6b04d5
shehuibianyuanren <155258874+shehuibianyuanren@users.noreply.github.com>
2024-07-19 20:52:37 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '670f9aee-5dad-4ba9-8de4-64cf8e187be7';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
